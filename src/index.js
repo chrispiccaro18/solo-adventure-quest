d93f9c73c21583db2b4009ac4724c3f3f6792e6b
Chris Piccaro <chrispiccaro18@gmail.com>
2019-02-14 14:48:44 -0800

    About to test produceHP function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('hi');
+const signInForm = document.getElementById('sign-in');
+
+signInForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const signInFormData = new FormData(signInForm);
+
+    // hitPoints = produceHitPoints(signInFormData.get('race'));
+
+    const userProfile = {
+        name: signInFormData.get('user-name'),
+        race: signInFormData.get('race')
+    };
+
+    console.log(userProfile);
+});
